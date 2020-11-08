cbeb5474c3370490d25904d6783e60d1af70e627
mohamedAyman008 <45742198+mohamedAyman008@users.noreply.github.com>
2020-11-08 16:45:55 +0200

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => MyApp();
+}
+class MyApp extends State<App> {
+  int counter = 0;
+  /* This widget is the root
+      of your application.*/
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        title: Text('MIU lost And Found'),
+        backgroundColor: Colors.red,
+      ),
+      body: 
+       
+       Column(children: [
+        Image.asset(
+                'images/MIU.jpg',
+                width: 350.4,
+                height: 200,
+                fit: BoxFit.fill,
+        ),
+        Row(
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+  children: const <Widget>[
+    Icon(
+      Icons.thumb_up,
+      color: Colors.green,
+      size: 150.0,   
+    ),
+    Icon(
+      Icons.thumb_down,
+      color: Colors.red,
+      size: 150.0,
+    ),
+  ]
+        ),
+Row(
+  mainAxisAlignment: MainAxisAlignment.spaceAround,
+  children: const <Widget>[
+   Text('I Found Something',
+    style: TextStyle( 
+      fontSize: 20,
+      fontWeight: FontWeight.w900),
+      ),
+      Text('I Lost Something',
+    style: TextStyle( 
+      fontSize: 20,
+      fontWeight: FontWeight.w900),
+      ),
+      ],
+      )
+      ],)
+        
+      ),
+      );
+}
+}
---
+++ lib/profile.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+  void main() => runApp(new MyApp());
+
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Stack Demo',
+        home: new StackDemo(),
+      );
+    }
+  }
+
+  class StackDemo extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Profile'),),
+        body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            // background image and bottom contents
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 200.0,color: Colors.orange,
+                  child: Center(
+                    child: Image.asset(
+                'images/photo2.jpg',
+                width: 440.4,
+               // height: 200,
+                fit: BoxFit.cover,
+        ),
+                    
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children:[ 
+                       
+                        Text('01003456789',
+                        style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange[300],
+                        ),
+                    ),
+                    Text('Malak Ahmed',
+                        style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[300],
+                        ),
+                    ),
+                    Text('Senior year faculty of Computer science',
+                        style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        ),
+                    )
+                      ],
+                )
+                )
+                )
+              ],
+            ),
+            // Profile image
+            Positioned(
+              top: 150.0,
+               // (background container size) - (circle height / 2)
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green,
+                  image:DecorationImage(
+                  image:new AssetImage('images/photo.jpg'),
+                  fit: BoxFit.fill,
+        )
+                ),
+              ),
+            ) 
+             ],
+        ),
+      );
+    }
+  }
